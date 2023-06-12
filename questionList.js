24487d5fed4bf52bf4ae0b8ce50eb87cf04fb5f3
hsagal78 <hsagal78@gmail.com>
2023-06-11 23:10:34 -0400

    add hove to buttons and innertext to the questions

---
+++ questionList.js
@@ -0,0 +1,227 @@
+var list_Questions = [
+    {
+        Question: 'Should we refer to JS as Jave?',
+        Choices: ['Maybe', 'No', 'Yes', 'Only Sunday'],
+        Answer: 'No'
+    },
+    
+    {
+        Question: 'Which property is used to change the font color in CSS?',
+        Choices: ['color', 'font-color', 'text-color', 'font-style'],
+        Answer: 'color' 
+    },
+    {
+        Question: 'Which keyword is used to declare a variable in JavaScript?',
+        Choices: ['let', 'variable', 'var', 'const'],
+        Answer: 'var'
+    },
+    {
+        Question: 'What does CSS stand for?',
+        Choices: ['Cascading Style Sheets', 'Creative Style Sheets', 'Computer Style Sheets', 'Colorful Style Sheets'],
+        Answer: 'Cascading Style Sheets'
+    },
+
+  
+    {
+        Question: 'Which property is used to control the spacing between lines of text in CSS?',
+        Choices: ['line-height', 'letter-spacing', 'word-spacing', 'text-spacing'],
+        Answer: 'line-height'
+    },
+    {
+        Question: 'What is the correct way to write a JavaScript comment?',
+        Choices: ['// This is a comment', '<!-- This is a comment -->', '/* This is a comment */', '// This is a comment //'],
+        Answer: '// This is a comment'
+    },
+    {
+        Question: 'What is the purpose of the HTML <head> tag?',
+        Choices: ['To define the main content of the document', 'To define the header section of the document', 'To define the footer section of the document', 'To define the metadata and document title'],
+        Answer: 'To define the metadata and document title'
+    },
+    {
+        Question: 'Which method is used to add a new element to the end of an array in JavaScript?',
+        Choices: ['push()', 'pop()', 'shift()', 'unshift()'],
+        Answer: 'push()'
+    },
+    {
+        Question: 'How can you center align a block element horizontally in CSS?',
+        Choices: ['text-align: center', 'align: center', 'margin: auto', 'float: center'],
+        Answer: 'margin: auto'
+    },
+    {
+        Question: 'What does the CSS property "display: none;" do?',
+        Choices: ['Hides an element', 'Changes the font color to none', 'Displays an element as inline', 'Changes the element to a block'],
+        Answer: 'Hides an element'
+    },
+    {
+        Question: 'Which HTML attribute is used to specify additional CSS styles for an element?',
+        Choices: ['class', 'style', 'id', 'src'],
+        Answer: 'style'
+    },
+    {
+        Question: ' What is the purpose of the JavaScript function "parseInt()"?',
+        Choices: ['To parse a string and return it as an integer', 'To check if a variable is an integer', 'To round a number to the nearest integer', 'To convert a number to a string'],
+        Answer: 'To parse a string and return it as an integer'
+    },
+    {
+        Question: 'Which CSS property is used to create rounded corners on an element?',
+        Choices: ['border-radius', 'corner-radius', 'round-corners', 'border-style'],
+        Answer: 'border-radius'
+    },
+    
+    {
+        Question: 'How do you write "Hello, World!" in an alert box using JavaScript?',
+        Choices: ['msg("Hello, World!");', 'document.alert("Hello, World!");', 'alertBox("Hello, World!");', 'alert("Hello, World!");'],
+        Answer: 'alert("Hello, World!");'
+    },
+    {
+        Question: ' What does the CSS property "position: absolute;" do?',
+        Choices: ['Positions an element relative to its nearest positioned ancestor', 'Positions an element absolutely within its containing element', 'Positions an element at the top left corner of the page', 'Positions an element relative to the mouse pointer'],
+        Answer: 'Positions an element absolutely within its containing element'
+    },
+    {
+        Question: ' Which operator is used to concatenate strings in JavaScript?',
+        Choices: ['+', '-', '*', '.'],
+        Answer: '+'
+    },
+    {
+        Question: 'What is the purpose of the HTML <footer> tag?',
+        Choices: ['To define a footer section for the document', 'To define a footer section for a table', 'To define the main content of the document', 'To define a footer section for a form'],
+        Answer: 'To define a footer section for the document'
+    },
+    {
+        Question: ' How do you select an element with the id "myElement" using CSS?',
+        Choices: ['#myElement', '.myElement', 'myElement', '*myElement'],
+        Answer: '#myElement'
+    },
+    {
+        Question: 'Which method is used to remove the last element from an array in JavaScript?',
+        Choices: ['pop()', 'push()', 'shift()', 'unshift()'],
+        Answer: 'pop()'
+    },
+    {
+        Question: ' What does the CSS property "text-decoration: underline;" do?',
+        Choices: ['Underlines the text', 'Changes the font to underline', 'Adds a border to the text', 'Aligns the text to the underline'],
+        Answer: 'Underlines the text'
+    },
+   
+  
+    {
+        Question: 'How can you include an external JavaScript file in your HTML document?',
+        Choices: ['<js src="script.js">', '<script src="script.js">', '<include src="script.js">', '<link rel="script" href="script.js">'],
+        Answer: '<script src="script.js">'
+    },
+    {
+        Question: 'What is the purpose of the CSS property "float"?',
+        Choices: ['To align an element to the left or right of its container', 'To make an element float above other elements', 'To change the opacity of an element', 'To animate an element'],
+        Answer: 'To align an element to the left or right of its container'
+    },
+    {
+        Question: 'What does the JavaScript method "toUpperCase()" do?',
+        Choices: ['Converts a string to lowercase', 'Converts a string to uppercase', 'Checks if a string is uppercase', 'Reverses the case of a string'],
+        Answer: 'Converts a string to uppercase'
+    },
+    {
+        Question: 'Which CSS property is used to change the background color of an element?',
+        Choices: ['background-color', 'color', 'background', 'bgcolor'],
+        Answer: 'background-color'
+    },
+  
+    {
+        Question: 'What is the purpose of the JavaScript function "addEventListener()"?',
+        Choices: ['To add an event handler to an element', 'To create a new event', 'To remove an event listener', 'To trigger an event'],
+        Answer: 'To add an event handler to an element'
+    },
+    {
+        Question: 'How do you write a single-line comment in CSS?',
+        Choices: ['/* This is a comment */', '// This is a comment', '<!-- This is a comment -->', '# This is a comment'],
+        Answer: '// This is a comment'
+    },
+    {
+        Question: 'What is the purpose of the HTML <nav> tag?',
+        Choices: ['To define navigation links', 'To define a section of navigation within the document', 'To define a navigation menu', 'To define a section of links'],
+        Answer: 'To define a section of navigation within the document'
+    },
+    {
+        Question: 'Which method is used to find the length of an array in JavaScript?',
+        Choices: ['length()', 'size()', 'count()', 'length'],
+        Answer: 'length'
+    },
+    {
+        Question: ' What does the CSS property "font-weight" control?',
+        Choices: ['The thickness of the text', 'The color of the text', 'The size of the text', 'The style of the text'],
+        Answer: 'The thickness of the text'
+    },
+
+   
+    {
+        Question: ' How do you write an if statement in JavaScript?',
+        Choices: ['if x = 5 then', 'if (x == 5)', 'if x == 5:', 'if x = 5'],
+        Answer: 'if (x == 5)'
+    },
+    {
+        Question: 'What is the purpose of the CSS property "text-align"?',
+        Choices: ['To align text vertically', 'To align text horizontally', 'To change the size of the text', 'To change the font of the text'],
+        Answer: 'To align text horizontally'
+    },
+    {
+        Question: 'Which JavaScript method is used to remove whitespace from both ends of a string?',
+        Choices: ['trim()', 'removeWhitespace()', 'replace()', 'trimWhitespace()'],
+        Answer: 'trim()'
+    },
+    {
+        Question: ' What does the HTML <aside> tag represent?',
+        Choices: ['A section of additional content', 'An image', 'A side navigation', 'A footer section'],
+        Answer: 'A section of additional content'
+    },
+    {
+        Question: ' How can you change the background image of an element using CSS?',
+        Choices: ['background-image', 'image-background', 'background-url', 'url-background'],
+        Answer: 'background-image'
+    },
+    {
+        Question: ' What does the JavaScript method "toLowerCase()" do?',
+        Choices: ['Converts a string to lowercase', 'Converts a string to uppercase', 'Checks if a string is lowercase', 'Reverses the case of a string'],
+        Answer: 'Converts a string to lowercase'
+    },
+    {
+        Question: 'Which CSS property is used to add space between elements?',
+        Choices: ['margin', 'padding', 'space', 'gap'],
+        Answer: 'margin'
+    },
+   
+    {
+        Question: ' How do you write a multi-line comment in JavaScript?',
+        Choices: ['/* This is a comment /', '// This is a comment', '<!-- This is a comment -->', '## This is a comment'],
+        Answer: '/ This is a comment */'
+    },
+    {
+        Question: ' What is the purpose of the CSS property "box-sizing"?',
+        Choices: ['To control the size of an element', 'To add a border to an element', 'To adjust the spacing between elements', 'To include padding and border in the elements total width and height'],
+        Answer: 'To include padding and border in the elements total width and height'
+    },
+    {
+        Question: 'Which method is used to concatenate arrays in JavaScript?',
+        Choices: ['concat()', 'join()', 'slice()', 'push()'],
+        Answer: 'concat()'
+    },
+    {
+        Question: 'What does the HTML <label> tag do?',
+        Choices: ['Defines a label for an input element', 'Defines a section of labels within a form', 'Defines a list of labels', 'Defines a caption for a table'],
+        Answer: 'Defines a label for an input element'
+    },
+    {
+        Question: 'How do you write a CSS rule that applies to elements with the class "myClass"?',
+        Choices: ['myClass { }', '.myClass { }', '<myClass> { }', '#myClass { }'],
+        Answer: '.myClass { }'
+    },
+    {
+        Question: 'What is the purpose of the JavaScript function "getElementById()"?',
+        Choices: ['To select an element by its class name', 'To select an element by its tag name', 'To select an element by its ID', 'To select an element by its name'],
+        Answer: 'To select an element by its ID'
+    },
+    {
+        Question: ' Which CSS property is used to control the spacing between letters in a text?',
+        Choices: ['letter-spacing', 'word-spacing', 'text-spacing', 'line-height'],
+        Answer: 'letter-spacing'
+    },
+];
