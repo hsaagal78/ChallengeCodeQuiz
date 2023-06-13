23524148693387045376b40ffd5ad7ae57f8967b
hsagal78 <hsagal78@gmail.com>
2023-06-12 23:36:50 -0400

    store score but no work .no show in the screen

--- script.js
+++ script.js
@@ -67,6 +67,7 @@ for (var i = 0; i < 2; i++) {
 
 //Funtion to start the quiz
 function startQuiz() {
+    starOneMoreTime.style.display = 'none';// hide the start the quiz
     resultsDiv.innerText = answerEachTime;
     firstPage.style.display = 'none';
     answerOption.style.display = "block";
@@ -139,60 +140,105 @@ function finisheQuiz() {
     studentInformation.style.display = 'block';
     resultsDiv.innerText = 'All done';
     resultsDiv.innerHTML += 'Your Final score is: ' + scoreTrue + '<br>';
+    starOneMoreTime.style.display = 'block';
+    startQ.style.display = 'block';
+    scoresDiv.style.display = 'block';
+  }
+///////////////////////////////////////////////////////////////////////
+ var starOneMoreTime= document.querySelector('#ClearScore');
+ var startQ= document.querySelector('.againQuiz');
+ var input = document.querySelector('#StudentInicial');
+ var scoresDiv = document.querySelector('.scores');
+ var sumit = document.querySelector('#studentInf');
+
+ function starAgain() {
+
+    startQuiz();
+
+ }
+ startQ.addEventListener('click', starAgain);
+
+  // this function shows when user wants to taken again the quiz and score new score will be save;
+  function clearData() { 
+    input.value = '';
+    scoreTrue= 0;
+    scoreFalse=0;
+
   }
 
-
-
-function saveUserData (arr){
-    var jsonVal = JSON.stringify(arr);
+  function getUserData() {
+    var rawData = localStorage.getItem(!input.value);
+    var parsed = JSON.parse(rawData) || [];
     
-    localStorage.setItem('user', jsonVal);
     
-}
-
-function saveUserClicks (evenObj) {
-    if(evenObj.keyCode ===13) {
-
-        var userScore ={
-            name: input.value,
-            clicks : clicks
+    return parsed;
+  }
+  
+  function saveStudentData(arr) {
+    var jsonArray = JSON.stringify(arr);
+    localStorage.setItem(input.value, jsonArray); // Changed key to 'users'
+   
+  }
+  
+  function saveUserClicks(eventObj) {
+   
+        var userScore = {
+          True: scoreTrue,
+          False: scoreFalse,
+          name: input.value
         };
-
-        var userArray = getUserData();
-        userArray.push(userScore);
-
-        saveUserData(userArray);
-        showScores();
-        clearData();
+   
+    var userArray = getUserData();
+    userArray.push(userScore);
+        
+    saveStudentData(userArray);
+    showScores();
+    clearData();
+    
     }
-}
+        
+  
+    sumit.addEventListener('click', saveUserClicks); 
+  
+    function showScores() {
+        var users = getUserData();
+        var scoresDiv = document.querySelector('.scores');
+      
+        scoresDiv.innerHTML = '';
+      
+        if (users.length === 0) {
+          scoresDiv.innerHTML = '<p> no scores have been added </p>';
+        }
+      
+        for (var userObj of users) {
+          var div = document.createElement('div');
+      
+          var h3 = document.createElement('h3');
+          h3.innerText = `Name: ${userObj.name}`; // Set content for h3
+          div.append(h3);
+      
+          var p = document.createElement('p');
+          p.innerText = `True: ${userObj.True}`; // Set content for p
+          div.append(p);
+      
+          var p2 = document.createElement('p');
+          p2.innerText = `False: ${userObj.False}`; // Set content for p2
+          div.append(p2);
+      
+          scoresDiv.append(div);
+        }
+      }
+    
+      
+      
+      
+      
+      
+      
+      
+    
 
-function showScores(){
-    var users = getUserData();
-    var scoresDiv = document.querySelector('scores');
-    scoresDiv.innerHTML ='';
-
-    if (users.length===0) {
-
-        scoresDiv.innerHTML = '<p> no scores have been added </p>';
-
-    }
-
-    for (var userObj of users) {
-        var div = document.createElement('div');
-        div.classList.add('score');
-
-        var h3 = document.createElement('h3');
-        div.innerText = 'Name: ${userObj.name}';
-        div.append(h3);
-        var p = document.createElement('p');
-        p.innerText = 'clicks: ${userObj. clicks}';
-        div.append(p);
-        scoresDiv.append(div);
-
-    }
-
-}
+showScores();
 
 
 
